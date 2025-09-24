53ded9347491711b707c6b983d432c63911e92a8
hu1 <3047853373@qq.com>
2025-09-25 01:05:21 +0800

    9.25

--- CPP/duoxiancheng/mynote.md
+++ CPP/duoxiancheng/mynote.md
@@ -1,29 +1,77 @@
-**join 和detach区别**,join后面的代码不会被执行除非子线程结束,而detach启动的线程在后台运行,当前代码继续执行不等待新线程结束
+***\*join 和detach区别\****,join后面的代码不会被执行除非子线程结束,而detach启动的线程在后台运行,当前代码继续执行不等待新线程结束
 
-​可以使用joinable判断是join模式还是detach模式。
 
-```text
+
+可以使用joinable判断是join模式还是detach模式。
+
+
+
+\```text
+
 if (myThread.joinable()) foo.join();
-```
 
-**this_thread**,四个功能函数为get_id(获取线程id)，yield(放弃线程执行，回到就绪状态)，sleep_for(暂停一秒)，sleep_until(一分钟之后执行吗，如下)
+\```
+
+
+
+***\*this_thread\****,四个功能函数为get_id(获取线程id)，yield(放弃线程执行，回到就绪状态)，sleep_for(暂停一秒)，sleep_until(一分钟之后执行吗，如下)
+
 例子：
+
 using std::chrono::system_clock;
+
 std::time_t tt = system_clock::to_time_t(system_clock::now());
+
 struct std::tm * ptm = std::localtime(&tt);
+
 cout << "Waiting for the next minute to begin...\n";
+
 ++ptm->tm_min; //加一分钟
+
 ptm->tm_sec = 0; //秒数设置为0//暂停执行，到下一整分执行
+
 this_thread::sleep_until(system_clock::from_time_t(mktime(ptm)));
 
-**mutex**
+
+
+***\*mutex\****
+
 常用操作：
+
 lock，unlock
+
 trylock{1 未上锁返回false，并锁住；2 其他线程已经上锁，返回true；3 同一个线程已经对他上锁，将会产生死锁}
 
+
+
 lock_guard
+
 创建lock_guard对象时，它将尝试获取提供给它的互斥锁的所有权。当控制流离开lock_guard对象的作用域时，lock_guard析构并释放互斥量。lock_guard的特点：
+
 创建即加锁，作用域结束自动析构并解锁，无需手工解锁
+
 不能中途解锁，必须等作用域结束才解锁
+
 不能复制
 
+**condition_variable**
+
+ 	condition_variable头文件有两个variable类，一个是condition_variable，另一个是condition_variable_any。condition_variable必须结合unique_lock使用。condition_variable_any可以使用任何的锁。
+
+
+
+## 线程池
+
+**概念**:在一个程序中，如果我们需要多次使用线程，这就意味着，需要多次的创建并销毁线程。而创建并销毁线程的过程势必会消耗内存，线程过多会带来调动的开销，进而影响缓存局部性和整体性能。线程的创建并销毁有以下一些缺点：
+
+1. 创建太多线程，将会浪费一定的资源，有些线程未被充分使用。
+
+2. 销毁太多线程，将导致之后浪费时间再次创建它们。
+
+3. 创建线程太慢，将会导致长时间的等待，性能变差。
+
+4. 销毁线程太慢，导致其它线程资源饥饿。
+
+线程池维护着多个线程，这避免了在处理短时间任务时，创建与销毁线程的代价。
+
+**实现**:
--- CPP/duoxiancheng/test4.cpp
+++ CPP/duoxiancheng/test4.cpp
@@ -1,38 +1,38 @@
-//常见生产者-消费者模型
-
-#include <iostream>           // std::cout
-#include <thread>             // std::thread, std::this_thread::yield
-#include <mutex>              // std::mutex, std::unique_lock
-#include <condition_variable> // std::condition_variable
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 
 std::mutex mtx;
 std::condition_variable cv;
 int cargo = 0;
-bool shipment_available() 
-{
-    return cargo!=0;
-}
-void consume (int n) 
-{
-    for (int i=0; i<n; ++i) 
-    {
-        std::unique_lock<std::mutex> lck(mtx);//自动上锁
-        //第二个参数为false才阻塞（wait），阻塞完即unlock，给其它线程资源
-        cv.wait(lck,shipment_available);// consume:
-        std::cout << cargo << '\n';
-        cargo=0;
+bool ready_to_consume = false; // 标记：是否有货物可以消费
+
+// 消费者
+void consume(int n) {
+    for (int i = 0; i < n; ++i) {
+        std::unique_lock<std::mutex> lck(mtx);
+        // 没有货物时等待
+        cv.wait(lck, [] { return ready_to_consume; });
+        std::cout << "Consume: " << cargo << '\n';
+        cargo = 0;
+        ready_to_consume = false;
+        cv.notify_one(); // 通知生产者可以继续生产
     }
 }
-int main ()
-{
-    std::thread consumer_thread (consume,10);
-    for (int i=0; i<10; ++i) 
-    {
-        //每次cargo每次为0才运行。
-        while (shipment_available())  std::this_thread::yield();
+
+// 生产者
+int main() {
+    std::thread consumer_thread(consume, 10);
+
+    for (int i = 0; i < 10; ++i) {
         std::unique_lock<std::mutex> lck(mtx);
-        cargo = i+1;
-        cv.notify_one();
+        // 如果上一次的货物还没消费，就等待
+        cv.wait(lck, [] { return !ready_to_consume; });
+        cargo = i + 1;
+        std::cout << "Produce: " << cargo << '\n';
+        ready_to_consume = true;
+        cv.notify_one(); // 通知消费者可以消费
     }
 
     consumer_thread.join();
